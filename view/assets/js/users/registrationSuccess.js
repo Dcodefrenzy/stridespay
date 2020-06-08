a632fc342094f9326e0a5e0fb342efd64dc68900
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2020-06-08 18:29:01 +0100

    added something to register page

--- view/assets/js/users/registrationSuccess.js
+++ view/assets/js/users/registrationSuccess.js
@@ -17,9 +17,10 @@ exports.registrationSuccess = (token, id)=>{
 												<p class="mt-5">Tell your friends about us and enjoy more discount?</p>
 												<p class="mt-3">For every friend you bring, you get, one month free discount. I.e 12 friends = One full year subcription.</p>
 
-										<input class="form-control" id=${id} type="text" value=${window.location.hostname+"/users/register/"+id} readonly="readonly"/>
-										<button onclick="return copyText(this.value)" value=${id} class="mt-2 col-12 col-sm-5 offset-sm-3 col-md-5 offset-md-3 btn-sm btn-green ">Copy</button>
-
+												<input class="form-control" id=${id} type="text" value=${window.location.hostname+"/users/register/"+id} readonly="readonly"/>
+												<button onclick="return copyText(this.value)" value=${id} class="mt-2 col-12 col-sm-5 offset-sm-3 col-md-5 offset-md-3 btn-sm btn-green ">Copy</button>
+												<p>We will get back to you when we launch. But before then, learn more about us <a href="/" class="btn-sm btn-strides">Here</a> </p>
+												
 											</div>
 											</div>
 										</div>
