7f3d4afb63af1da6e83ab771f8e0e3c61aa176f1
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2020-06-08 04:44:00 +0100

    coupon added

--- view/assets/js/signUp.js
+++ view/assets/js/signUp.js
@@ -5,13 +5,13 @@ define(function(require, exports, module) {
 	const spinner = document.getElementById("spinner");
 	spinner.className ="display-none";
 
-	const html = `<div id="login" class=" bg-green min-height">
+	const html = `<div id="login" class=" bg-navy min-height">
 			<div class="container">
 					<div class="row align-items-center p-2">
 		<div class="col-12 col-sm-12 col-md-12 col-lg-12">
 				<a href="/"><i class="fa fa-arrow-left text-dark"></i></div></a>
 				<div class="col-12 col-sm-12 col-md-6 col-lg-6 mt-lg-5">
-					<img  src="/assets/images/strides1.png"></div>
+					<img  src="/assets/images/strides.png"></div>
 				<div class="col-12 col-sm-12 col-md-6 col-lg-6">
 					<h1>Sign Up</h1>
 					<form id=${`users/subscribe/${id}`} class="signUpUser" name="submitForm" onsubmit="return register(event)">
@@ -52,7 +52,7 @@ define(function(require, exports, module) {
 							</div>
 							<div class="col-12 col-sm-12 col-md-12">
 								<div class="form-group">
-									<input type="submit" name="submit" class="form-control btn-lg btn-success" value="Signup">
+									<input type="submit" name="submit" class="form-control btn-lg btn-strides" value="Signup">
 								</div>
 							<div>
 						</div>
--- view/assets/js/users/subscribers.js
+++ view/assets/js/users/subscribers.js
@@ -1,53 +1,53 @@
 define(function(require, exports, module) {
 	let subscribersForm;
 
-	exports.subscribersForm = `<div id="revert" class=" bg-green min-height">
+	exports.subscribersForm = `<div id="revert" class=" bg-navy min-height">
 				<div class="container">
 					<div class="row mt-2 p-3">
 					<div class="col-12 col-sm-12 col-md-12 col-lg-12">
 				<a href="/"><i class="fa fa-arrow-left text-dark"></i></div></a>
 					</div>
 				<div class="col-12 col-sm-12 col-md-12 col-lg-12">
-					<h3>Register and get free cupons when we launch</h3>
+					<h3>Register and get free coupons when we launch</h3>
 					<form id="users/signup" class="signUpUser" name="submitForm" onsubmit="return register(event)">
 						<div class="row">
 							<p id="error-message"></p>
 							<div class="col-12 col-sm-12 col-md-6">
-								<div class="form-group text-white">
+								<div class="form-group text-strides">
 									<label id="error-message"></label>
 									<label id="error-email">Email</label>
 									<input type="email" name="email" oninput="return returnValidation(this.value, this.name)"  class="form-control" placeholder="Enter your email here" required>
 								</div>
 							</div>
 							<div class="col-12 col-sm-12 col-md-6">
-								<div class="form-group text-white">
+								<div class="form-group text-strides">
 									<label id="error-firstname"></label>
 									<label id="error-firstname">Firstname</label>
 									<input type="text" name="firstname" oninput="return returnValidation(this.value, this.name)"  class="form-control" placeholder="Enter your firstname here" required>
 								</div>
 							</div>
 							<div class="col-12 col-sm-12 col-md-6">
-								<div class="form-group text-white">
+								<div class="form-group text-strides">
 									<label id="error-lastname"></label>
 									<label id="error-firstname">Lastname</label>
 									<input type="text" name="lastname" oninput="return returnValidation(this.value, this.name)"  class="form-control" placeholder="Enter your lastname here" required>
 								</div>
 							</div>
 							<div class="col-12 col-sm-12 col-md-6">
-								<div class="form-group text-white">
+								<div class="form-group text-strides">
 									<label id="error-message"></label>
 									<label id="error-phonenumber">Phone Number</label>
 									<input type="text" name="phonenumber" oninput="return returnValidation(this.value, this.name)"  class="form-control" placeholder="Enter your number here" required>
 								</div>
 							</div>
 							<div class="col-12 col-sm-12 col-md-6">
-								<div class="form-group text-white">
+								<div class="form-group text-strides">
 									<label id="error-password">Password</label>
 									<input type="password" name="password" oninput="return returnValidation(this.value, this.name)" class="form-control" placeholder="Enter your password here" required></div>
 							</div>
 							<div class="col-12 col-sm-12 col-md-6">
 								<div class="form-group">
-									<input type="submit" name="submit" class="form-control btn-lg btn-success" value="Signup">
+									<input type="submit" name="submit" class="form-control btn-lg btn-strides" value="Signup">
 								</div>
 							<div>
 						</div>
