e3f652df7cd4d060e33079ae4055b511752d1a5c
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2021-02-20 00:21:58 +0100

    Building stridespay
    	Redesign all page components which includes
    		Dashboard
    		Profile
    		Project
    		Transactions
    		Payments
    		Client Database
    		Analytics
    		Milestones
    		Contracts and many more.
    
    		Rolling out to users for a beta test.

--- view/assets/js/users/freelancers/createMilestone.js
+++ view/assets/js/users/freelancers/createMilestone.js
@@ -6,47 +6,61 @@ define(function(require, exports, module) {
 	const spinner = document.getElementById(close);
 
 	spinner.className ="display-none";
-		const html = `<div id="revert" class="bg-background container">
-				<div class="container">
-				<div id="loading"></div>
-					<div class="row mt-2 p-3">
-					<div class="col-12 col-sm-12 col-md-9 offset-md-3 col-lg-9 offset-lg-3">
-				<a href=${"/users/services/"+id}><i class="fa fa-arrow-left text-dark"></i></div></a>
-					</div>
-				<div class="col-12 col-sm-12 col-md-9 offset-md-3 col-lg-9 offset-lg-3">
-					<h1>Create Milestone</h1>
-					<p class="text-dark">Create a Milestone</p>
-					<form id=${"milestones/create/"+id} class="createMilestone" name="submitForm" onsubmit="return register(event)">
-						<div class="row">
-							<div class="col-12 col-sm-12 col-md-12">
-								<div class="form-group">
-									<label id="error-title">Title</label>
-									<input type="text" name="title" required oninput="return returnValidation(this.value, this.name)"  class="form-control" placeholder="eg design a prototype?">
-								</div>
-							</div>
-							<div class="col-12 col-sm-12 col-md-12">							
-								<div class="form-group">
-									<label id="error-price">What is the price for this milestone?</label>
-									<input type="Number" name="price" required oninput="return returnValidation(this.value, this.name)"  class="form-control" placeholder="The  amount for this milestone?">
-								</div>
-							</div>
-							<div class="col-12 col-sm-12 col-md-12">							
-								<div class="form-group" id='textarea-div'>
-									<label id="error-description">Description</label>	
-									
-								</div>
-							</div>
-							<div class="col-12 col-sm-12 col-md-12">
-								<div class="form-group">
-									<input type="submit" name=${id} class="form-control btn-lg btn-dark" value="Save">
-								</div>
-							<div>
-						</div>
-					</form>
+		const html = `<div id="revert" class="dsh-content-wrapper col-12 col-sm-12 col-md-9 offset-md-3 col-lg-10 offset-lg-2 col-xl-10 offset-xl-1">
+
+			<!-- Breadcrumbs -->
+			<div class="breadcrumb-wrapper d-flex align-items-start align-items-sm-center justify-content-between flex-column flex-sm-row">
+				<div class="breadcrumb-wrapper--inner d-flex flex-column-reverse">
+					<h1>Service</h1>
+					<ol class="breadcrumb style-1">
+						<li class="breadcrumb-item"><a href="/users/dashboard">Dashboard</a></li>
+						<li class="breadcrumb-item"><a href="/users/services/${id}">Service</a></li>
+						<li class="breadcrumb-item" aria-current="page">Milestone</li>
+					</ol>
 				</div>
 			</div>
-		</div>
-	 </div>`;
+
+			<div class="row">
+
+				<div class="col-xl-12">
+					<div class="card">
+						<div class="card-body">
+							<h1>Create A milestones</h1>
+							<p >Create a Milestone</p>
+							<form id=${"milestones/create/"+id} class="createMilestone" name="submitForm" onsubmit="return register(event)">
+								<div class="row">
+									<div class="col-12 col-sm-12 col-md-12">
+										<div class="form-group">
+											<label id="error-title">Title</label>
+											<input type="text" name="title" required oninput="return returnValidation(this.value, this.name)"  class="form-control" placeholder="eg design a prototype?">
+										</div>
+									</div>
+									<div class="col-12 col-sm-12 col-md-12">							
+										<div class="form-group">
+											<label id="error-price">What is the price for this milestone?</label>
+											<input type="Number" name="price" required oninput="return returnValidation(this.value, this.name)"  class="form-control" placeholder="The  amount for this milestone?">
+										</div>
+									</div>
+									<div class="col-12 col-sm-12 col-md-12">							
+										<div class="form-group" id='textarea-div'>
+											<label id="error-description">Description</label>	
+											
+										</div>
+									</div>
+									<div class="col-12 col-sm-12 col-md-12">
+										<div class="form-group">
+											<input type="submit" name=${id} class="form-control btn-lg btn-dark" value="Save">
+										</div>
+									<div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</div>`;
 	 body.insertAdjacentHTML('beforeend', html);
 	 displayTextEditor()
 	}
